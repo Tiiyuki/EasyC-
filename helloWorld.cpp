265dd72148c983f2e4581f67baba06d74757b50d
Yuki　Git <yt.shingeki@gmail.com>
2025-03-16 12:11:52 +0900

    first commit

---
+++ helloWorld.cpp
@@ -0,0 +1,7 @@
+/* 確認用プログラム */
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello,World!\nおつかれさまでした！\n");
+	return 0;
+}
---
+++ sample.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "ようこそC＋＋へ!\n";
+    cout << "c++をはじめましょう！\n";
+
+    return 0;
+
+}
