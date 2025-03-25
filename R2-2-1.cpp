579cbb3db331c8dda77b0eb141d417bd213b6921
Yuki　Git <yt.shingeki@gmail.com>
2025-03-25 19:06:41 +0900

    新しいCPPファイルを追加

---
+++ R2-2-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << 1 << 2 << 3 << '\n' << 4 << 5 << '\n';
+    //123と45をわけて表示する
+  
+    
+    
+
+    return 0;
+
+}
---
+++ R2-4-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << 6 << '\n';//8進数
+    cout << 6 << '\n';//16進数
+
+    cout << 24 << '\n';//8進数
+    cout << 16 << '\n';//16進数
+
+    cout << 15 << '\n';//8進数
+    cout << 0xD << '\n';//16進数
+
+    
+  
+    
+    
+
+    return 0;
+
+}
